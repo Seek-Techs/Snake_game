30428077014a02307437efc98183572de0169b38
Sikiru Yusuff <sikiru.yusuff@yahoo.com>
2022-10-17 16:27:06 +0100

    update

--- snake.py
+++ snake.py
@@ -97,73 +97,73 @@ while True:
         # hide the segments of body
         for segment in segments:
             segment.goto(1000, 1000) # out of range
-            # clear the segments
+        # clear the segments
+        segments.clear()
+
+        # reset score
+        score = 0
+
+        # reset delay
+        delay = 0.1
+
+        sc.clear()
+        sc.write('score: {} High score: {}'.format(score, high_score), align = 'center', font = ('ds-digital', 24, 'normal'))
+
+    # check collision with food
+    if head.distance(food) < 20:
+        # move the food to random place
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        # add a new segment to the head
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape('square')
+        new_segment.color('black')
+        new_segment.penup()
+        segments.append(new_segment)
+
+        #shorten the delay
+        delay -= 0.001
+        # increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        sc.clear()
+        sc.write('score: {} High score: {}'.format(score, high_score), align = 'center', font=('ds-digital', 24, 'normal'))
+
+    # move the segments in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index -1].xcor()
+        y = segments[index -1].ycor()
+        segments[index].goto(x,y)
+
+    # move segment 0 to head
+    if len (segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+    move()
+
+    # check for collision with body
+    for segment in segments:
+        if segment.distance(head) < 20 :
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = 'stop'
+
+            # hide segments
+            for segment in segments:
+                segment.goto(1000, 1000)
             segments.clear()
-
-            # reset score
             score = 0
-
-            # reset delay
             delay = 0.1
-
+            # update the score
             sc.clear()
             sc.write('score: {} High score: {}'.format(score, high_score), align = 'center', font = ('ds-digital', 24, 'normal'))
+    time.sleep(delay)
 
-        # check collision with food
-        if head.distance(food) < 20:
-            # move the food to random place
-            x = random.randint(-290, 290)
-            y = random.randint(-290, 290)
-            food.goto(x, y)
-
-            # add a new segment to the head
-            new_segment = turtle.Turtle()
-            new_segment.speed(0)
-            new_segment.shape('square')
-            new_segment.color('black')
-            new_segment.penup()
-            segments.append(new_segment)
-
-            #shorten the delay
-            delay -= 0.001
-            # increase the score
-            score += 10
-
-            if score > high_score:
-                high_score = score
-                sc.clear()
-                sc.write('score: {} High score: {}'.format(score, high_score), align = 'center', font = ('ds-digital', 24, 'normal'))
-
-        # move the segments in reverse order
-        for index in range(len(segments)-1, 0, -1):
-            x = segments[index -1].xcor()
-            y = segments[index -1].ycor()
-            segments[index].goto(x,y)
-
-        # move segment 0 to head
-        if len (segments) > 0:
-            x = head.xcor()
-            y = head.ycor()
-            segments[0].goto(x, y)
-
-        move()
-
-        # check for collision with body
-        for segment in segments:
-            if segment.distance(head) < 20 :
-                time.sleep(1)
-                head.goto(0, 0)
-                head.direction = 'stop'
-
-                # hide segments
-                for segment in segments:
-                    segment.goto(1000, 1000)
-                segments.clear()
-                score = 0
-                delay = 0.1
-                # update the score
-                sc.clear()
-                sc.write('score: {} High score: {}'.format(score, high_score), align = 'center', font = ('ds-digital', 24, 'normal'))
-        time.sleep(delay)
-
-    wn.mainloop()
+#wn.mainloop()
